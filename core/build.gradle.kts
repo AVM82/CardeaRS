755644c708f215138bbc22e06b80ca02d964f23c
alexandr <1982alexvm@gmail.com>
2021-12-05 12:35:23 +0200

    refactored build.gradle.kts of core module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,36 +1,8 @@
-import org.rs.cardears.ConfigData
 import org.rs.cardears.Deps
 
 plugins {
-    id("com.android.library")
-    id( "kotlin-android")
-}
-
-android {
-    compileSdk = ConfigData.compileSdkVersion
-
-    defaultConfig {
-        minSdk = ConfigData.minSdkVersion
-        targetSdk = ConfigData.targetSdkVersion
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    id("java-library")
+    id("kotlin")
 }
 
 dependencies {
@@ -38,7 +10,10 @@ dependencies {
     implementation(Deps.coroutines)
 
     testImplementation(Deps.Test.junit)
-    androidTestImplementation(Deps.Test.junitUi)
-    androidTestImplementation(Deps.Test.espresso)
 
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
